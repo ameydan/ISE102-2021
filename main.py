4e1a3e861e1c31cd627a4e3bafbcab04a68b6454
Tolga Ovatman <ovatman@itu.edu.tr>
2021-10-21 12:27:57 +0300

    Exercise session

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #from L1008 import Variables
 #from L1008 import Branching
 #from L1014 import Exercises
-from L1015 import Loops
+#from L1015 import Loops
+from L1021 import Exercises
 
 #if __name__=="__main__":
 #    pass
